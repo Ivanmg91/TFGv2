62a964fbee09efd4e8f2dc590de1e5beedafa721
Iván Montiano González <160036159+Ivanmg91@users.noreply.github.com>
2025-04-11 10:09:52 +0000

    update

--- streaming-calculator/src/App.js
+++ streaming-calculator/src/App.js
@@ -19,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-        <text>Hola</text>
+        <text>Encestando con Patrick cada tirito que tengo (Vicentin)</text>
       </header>
     </div>
   );
