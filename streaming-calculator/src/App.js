e74f31719adbf7bd5a7d384667a5448490e10103
Iván Montiano González <160036159+Ivanmg91@users.noreply.github.com>
2025-04-15 07:17:12 +0000

    Streaming Calculator v0

--- streaming-calculator/src/App.js
+++ streaming-calculator/src/App.js
@@ -1,26 +1,46 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import * as streamingAvailability from "streaming-availability";
-
+import * as api from './api.js';
 
 function App() {
+  const [title, setTitle] = useState(''); // Estado para almacenar el título
+  const [movies, setMovies] = useState([]); // Estado para almacenar las películas
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await api.getData(); // Llama a la función getData
+      setTitle(data); // Actualiza el estado con el título
+    }
+    fetchData();
+
+    async function fetchMovies() {
+      const movieData = await api.getShows(); // Llama a la función getShows
+      setMovies(movieData); // Actualiza el estado con las películas
+    }
+    fetchMovies();
+  }, []); // Ejecuta solo una vez al montar el componente
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <text>Encestando con Patrick cada tirito que tengo (Vicentin)</text>
+        <h1>Información de la Película</h1>
+        {title ? <p>{title}</p> : <p>Cargando...</p>} {/* Muestra el título o un mensaje de carga */}
       </header>
+      <div className="movie-grid">
+        {movies.length > 0 ? (
+          movies.map((movie, index) => (
+            <div className="card" key={index}>
+              <img src={movie.poster} alt={movie.title} />
+              <div className="card-content">
+                <h3 className="card-title">{movie.title}</h3>
+                <p className="card-description">{movie.description}</p>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>Cargando películas...</p>
+        )}
+      </div>
     </div>
   );
 }
