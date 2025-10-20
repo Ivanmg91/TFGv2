8cf15209c37eb8fe3cd5ee6c0eb4cfa50b2cbf41
Iván Montiano González <160036159+Ivanmg91@users.noreply.github.com>
2025-10-20 13:14:28 +0200

    Update YouTube API key in ytApi.js

--- streaming-calculator/src/ytApi.js
+++ streaming-calculator/src/ytApi.js
@@ -6,7 +6,7 @@ export async function buscarTrailerYouTube(titulo, anio) {
   const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': '4654dd0d3cmsh03796de88e5c5d2p1a75c0jsnee7caa5b9852',
+      'X-RapidAPI-Key': '8a8b283431mshef5bc9da2289a66p1f771ajsnec84c74c8509',
       'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
     }
   };
